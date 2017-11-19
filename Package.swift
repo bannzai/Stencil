a29637fb937ce03788462e650d0deae8b9f4468f
bannzai <kingkong999yhirose@gmail.com>
2017-11-20 01:12:26 +0900

    :up: Package.swift adapt for swift version 4

--- Package.swift
+++ Package.swift
@@ -1,11 +1,27 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
   name: "Stencil",
+  products: [
+      // Products define the executables and libraries produced by a package, and make them visible to other packages.
+      .library(
+          name: "Stencil",
+          targets: ["Stencil"]),
+  ],
   dependencies: [
-    .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
+    .package(url: "https://github.com/kylef/PathKit.git", from: Version(0, 8, 0)),
 
     // https://github.com/apple/swift-package-manager/pull/597
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+    .package(url: "https://github.com/kylef/Spectre.git", from: Version(0, 7, 2)),
+  ],
+  targets: [
+      // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+      // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+      .target(
+         name: "Stencil",
+         dependencies: ["PathKit", "Spectre"]),
   ]
 )
